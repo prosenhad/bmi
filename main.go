57c81ea5003e02bdf567910c6c073f0e83162a10
andrey <prosenhad@gmail.com>
2025-01-15 15:33:14 +0300

    rework if-else on switch-case

--- main.go
+++ main.go
@@ -18,21 +18,22 @@ func calculateBMI(height float64, mass float64) float64 {
 
 // Функция для вывода результата
 func outputResult(bmi float64) {
-	if bmi < 16 {
+	switch {
+	case bmi < 16:
 		fmt.Printf("Индекс %.2f является показателем сильного дефицита массы тела\n", bmi)
-	} else if bmi < 18.5 {
+	case bmi < 18.5:
 		fmt.Printf("Индекс %.2f является показателем дефицита массы тела\n", bmi)
-	} else if bmi < 25 {
+	case bmi < 25:
 		fmt.Printf("Индекс %.2f является показателем нормальной массы тела \n", bmi)
-	} else if bmi < 30 {
+	case bmi < 30:
 		fmt.Printf("Индекс %.2f является показателем избыточной массы тела \n", bmi)
-	} else if bmi < 35 {
+	case bmi < 35:
 		fmt.Printf("Индекс %.2f является показателем 1-ой степени ожирения \n", bmi)
-	} else if bmi < 40 {
+	case bmi < 40:
 		fmt.Printf("Индекс %.2f является показателем 2-ой степени ожирения \n", bmi)
-	} else if bmi >= 40 {
+	case bmi >= 40:
 		fmt.Printf("Индекс %.2f является показателем 3-ой степени ожирения \n", bmi)
-	} else {
+	default:
 		log.Fatal(errors.New("неверные данные"))
 	}
 }
